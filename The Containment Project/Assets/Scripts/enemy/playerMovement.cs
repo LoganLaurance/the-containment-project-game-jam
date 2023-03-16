ca94f8f38fdc0a315f42330e148d38e96b17deb9
VolksxbWagen <113216796+VolksxbWagen@users.noreply.github.com>
2023-03-16 00:07:51 -0700

    player movement

--- The Containment Project/Assets/Scripts/enemy/playerMovement.cs
+++ The Containment Project/Assets/Scripts/enemy/playerMovement.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public GameObject player;
-    public float runspeed = 20.0f;
+    public float runspeed = 5.0f;
     public float health;
-
+    public Camera cam;
     Rigidbody2D rb;
     private float horizontal;
     private float vertical;
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +25,16 @@ public class playerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * runspeed, vertical * runspeed);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
