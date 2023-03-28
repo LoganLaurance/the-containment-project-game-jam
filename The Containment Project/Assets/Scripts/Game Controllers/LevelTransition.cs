39d5c4c4d7ef44de9380352273ebf1f0c716482b
Logan Laurance <logan.laurance@digipen.edu>
2023-03-28 15:56:33 -0700

    Updates

--- The Containment Project/Assets/Scripts/Game Controllers/LevelTransition.cs
+++ The Containment Project/Assets/Scripts/Game Controllers/LevelTransition.cs
@@ -14,10 +14,10 @@ using UnityEngine.SceneManagement;
 public class LevelTransition : MonoBehaviour
 {
     [Tooltip("Put scene name to transition to here. Case-sensitive!")]public string levelDestination;
+    [Tooltip("Put the open door sprite here. Will be different depending on the position of this object.")] public Sprite openState;
 
     private void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
 
         if(SceneManager.GetActiveScene().name == "Lobby")
@@ -28,7 +28,7 @@ public class LevelTransition : MonoBehaviour
 
     public void EnableTriggers()
     {
-        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<SpriteRenderer>().sprite = openState;
         gameObject.GetComponent<BoxCollider2D>().enabled = true;
     }
 
