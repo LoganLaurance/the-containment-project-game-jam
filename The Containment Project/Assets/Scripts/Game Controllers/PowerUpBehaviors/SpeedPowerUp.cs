bfc5c2c16c71735c71b6160d9ec5513ecc9f80da
Logan Laurance <logan.laurance@digipen.edu>
2023-03-28 18:01:22 -0700

    Speed PowerUp Update

--- The Containment Project/Assets/Scripts/Game Controllers/PowerUpBehaviors/SpeedPowerUp.cs
+++ The Containment Project/Assets/Scripts/Game Controllers/PowerUpBehaviors/SpeedPowerUp.cs
@@ -14,6 +14,15 @@ public class SpeedPowerUp : PowerUp
 {
     override public void UpdateTempStats()
     {
+        playerMovement player = FindObjectOfType<playerMovement>();
+        if (player != null)
+        {
+            if(player.runspeed >= player.speedCap)
+            {
+                Debug.Log("Cannot give this because we are at capped value.");
+                return;
+            }
+        }
         gm.AddPlayerSpeed(value);
         gm.UpdateInternalTempStats();
         gm.UpdateUIStatsText();
@@ -21,16 +30,22 @@ public class SpeedPowerUp : PowerUp
 
     public override void UpdatePermaStats(int currency)
     {
+        playerMovement player = FindObjectOfType<playerMovement>();
         if (currency < price)
         {
             Debug.Log("Not enough currency to pay for this!");
             return;
         }
-        else
+        else if(player != null)
         {
-            gm.SetCurrency(currency - price);
-            gm.AddSpeedBoost(value);
-            gm.UpdatePlayerPermaStats();
+            if(player.runspeed >= player.speedCap)
+            {
+                Debug.Log("Cannot buy this as we are at capped value.");
+                return;
+            }
         }
+        gm.SetCurrency(currency - price);
+        gm.AddSpeedBoost(value);
+        gm.UpdatePlayerPermaStats();
     }
 }
